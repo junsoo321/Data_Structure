5e7fcf2550d78c1136574b9ca1a8ec8a395eaa5a
undefinded <84854721+junsoo321@users.noreply.github.com>
2026-03-19 09:44:38 +0900

    Update 과제2.c

--- 202110931 박준수/3주차/과제2.c
+++ 202110931 박준수/3주차/과제2.c
@@ -3,11 +3,10 @@
 
 int main() {
 	int array[2][3][4];
-	int val = 1;
 	for (int i = 0;i <= 1;i++)
 		for (int j = 0;j <= 2;j++)
 			for (int k = 0;k <= 3;k++)
-				array[i][j][k] = val++;
+				scanf("%d",&array[i][j][k]);
 
 	for (int i = 0;i <= 1;i++)
 		for (int j = 0;j <= 2;j++)
