76bd310ca5e978186f099535f131589a00c2a413
undefinded <84854721+junsoo321@users.noreply.github.com>
2026-04-01 01:22:37 +0900

    Update LinkedList.h

--- 202110931 박준수/4주차/실습2/LinkedList.h
+++ 202110931 박준수/4주차/실습2/LinkedList.h
@@ -1,11 +1,10 @@
 #pragma once
-// 단순 연결 리스트의 노드 구조를 구조체로 정의
+
 typedef struct ListNode {
 	char data[4];
 	struct ListNode* link;
 } listNode;
 
-// 리스트의 시작을 나타내는 head 노드를 구조체로 정의
 typedef struct {
 	listNode* head;
 } linkedList_h;
