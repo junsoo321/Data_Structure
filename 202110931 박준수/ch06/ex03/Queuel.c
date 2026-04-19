a09e0e988f00565824fecd05e0b07820b6e0fe97
undefinded <84854721+junsoo321@users.noreply.github.com>
2026-04-19 18:30:04 +0900

    Create Queuel.c

---
+++ 202110931 박준수/ch06/ex03/Queuel.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "QueueL.h"
+
+queue* cQueue(void) {
+	queue* que;
+	que = (queue*)malloc(sizeof(queue));
+	que->front = NULL;
+	que->back = NULL;
+	return que;
+}
+
+int isEmpty(queue* que) {
+	return (que->front == NULL) ? 1 : 0;
+}
+
+void errorPrint() {
+	printf("Circular Queue is Empty!");
+}
+
+void addQueue(queue* que, char* c) {
+	queuetype* newnode = (queuetype*)malloc(sizeof(queuetype));
+	newnode->data = c;
+	newnode->link = NULL;
+	if (que->front == NULL) {
+		que->front = newnode;
+		que->back = newnode;
+	}
+	else {
+		que->back->link = newnode;
+		que->back = newnode;
+	}
+}
+
+char popQueue(queue* que) {
+	if (isEmpty(que)) {
+		errorPrint();
+		return;
+	}
+	queuetype* old = que->front;
+	char c;
+	c = old->data;
+	que->front = que->front->link;
+	if (que->front == NULL)
+		que->back = NULL;
+	free(old);
+	return c;
+}
+
+char peekQueue(queue* que) {
+	if (isEmpty(que)) {
+		errorPrint();
+		return;
+	}
+	return que->front->data;
+}
+
+void pQueue(queue* que) {
+	queuetype* temp = que->front;
+	printf(" Linked Queue:[ ");
+	while (temp) {
+		printf("%c ", temp->data);
+		temp = temp->link;
+	}
+	printf("]");
+}
