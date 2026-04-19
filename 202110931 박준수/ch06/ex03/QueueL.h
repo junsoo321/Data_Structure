89921089c12e8361d80f162d4769cc2b34eed283
undefinded <84854721+junsoo321@users.noreply.github.com>
2026-04-19 18:29:49 +0900

    Add QueueL.h header file for queue implementation

---
+++ 202110931 박준수/ch06/ex03/QueueL.h
@@ -0,0 +1,17 @@
+#pragma once
+
+typedef struct queuetype {
+	char data;
+	struct queue* link;
+} queuetype;
+
+typedef struct {
+	queuetype* front, * back;
+}queue;
+
+queue* cQueue(void);
+int isEmpty(queue* que);
+void addQueue(queue* que, char* c);
+char popQueue(queue* que);
+char peekQueue(queue* que);
+void pQueue(queue* que);
