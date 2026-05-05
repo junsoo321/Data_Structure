4a502cf337ebf2480642ceb0d45e116cc42dd4d7
undefinded <84854721+junsoo321@users.noreply.github.com>
2026-05-06 00:09:26 +0900

    Create traversalBT.h

---
+++ 202110931 박준수/ch07/ex01/traversalBT.h
@@ -0,0 +1,12 @@
+#pragma once
+
+typedef struct treeNode {	// 연결 자료구조로 구성하기 위해 트리의 노드 정의
+	char data;
+	struct treeNode* left;  // 왼쪽 서브 트리에 대한 링크 필드
+	struct treeNode* right; // 오른쪽 서브 트리에 대한 링크 필드
+} treeNode;
+
+treeNode* makeRootNode(char data, treeNode* leftNode, treeNode* rightNode);
+void preorder(treeNode* root);
+void inorder(treeNode* root);
+void postorder(treeNode* root);
